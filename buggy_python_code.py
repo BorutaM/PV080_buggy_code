f0493bcc2ff7241b04de02d80552b9a11fc7e847
BorutaM <borut.matej@gmail.com>
2026-05-05 10:43:36 +0200

    removed trailing whitespaces

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -25,7 +25,7 @@ def print_nametag(format_string, person):
 
 def fetch_website(urllib_version, url):
     # Import the requested version (2 or 3) of urllib
-    exec(f"import urllib{urllib_version} as urllib", globals())
+    exec(f"import urllib{urllib_version} ", globals()) as urllib
     # Fetch and print the requested URL
  
     try: 
@@ -51,7 +51,7 @@ if __name__ == '__main__':
     print("2. Code injection vulnerability:")
     print("3. Yaml deserialization vulnerability:")
     print("4. Use of assert statements vulnerability:")
-    choice  = input("Select vulnerability: ")
+    choice = input("Select vulnerability: ")
     if choice == "1": 
         new_person = Person("Vickie")  
         print_nametag(input("Please format your nametag: "), new_person)
